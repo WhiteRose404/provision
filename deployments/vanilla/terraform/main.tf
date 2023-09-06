e8a7aab59c2d21869cf2b5c19e533de8e2add27f
WhiteRose404 <eddoughri.youssef@gmail.com>
2023-09-06 18:19:02 +0100

    fix the addon issue

--- deployments/vanilla/terraform/main.tf
+++ deployments/vanilla/terraform/main.tf
@@ -105,7 +105,7 @@ data "aws_ec2_instance_type_offerings" "availability_zones_gpu" {
 # EKS Blueprints
 #---------------------------------------------------------------
 module "eks_blueprints" {
-  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.31.0"
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.30.0"
 
   cluster_name    = local.cluster_name
   cluster_version = local.eks_version
